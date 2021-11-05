795345ea4e7004595604301eaef11cacf935e2b4
Rituparna Warwatkar <rituparnaw16@gmail.com>
2021-11-05 21:29:12 +0530

    kek

--- 07-express-data-save/routes/home.js
+++ 07-express-data-save/routes/home.js
@@ -1,24 +1,39 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const router = express.Router();
 
 const Person = require('./../models/Person');
-
-const personArray = [];
 
 router.get('/', (req, res) => {
   res.render('form');
 });
 
 router.get('/data', (req, res) => {
+  const exisitingData = fs.readFileSync(
+    path.join(__dirname, '..', 'data', 'person.json')
+  );
   return res.render('data', {
-    parr: personArray,
+    parr: JSON.parse(exisitingData),
   });
 });
 
 router.post('/save', (req, res) => {
+  // const p = new Person(req.body.name, req.body.age);
+  // personArray.push(p);
+  // console.log(personArray);
+  // return res.redirect('/data');
+  const exisitingData = fs.readFileSync(
+    path.join(__dirname, '..', 'data', 'person.json')
+  );
+  // console.log(JSON.parse(exisitingData));
+  const arr = JSON.parse(exisitingData);
   const p = new Person(req.body.name, req.body.age);
-  personArray.push(p);
-  console.log(personArray);
+  arr.push(p);
+  fs.writeFileSync(
+    path.join(__dirname, '..', 'data', 'person.json'),
+    JSON.stringify(arr)
+  );
   return res.redirect('/data');
 });
 
